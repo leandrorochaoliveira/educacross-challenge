30a77289c8bdde758bfe54753d00b1950af4faca
leandrorochaoliveira <leandrorochaoliveira@icloud.com>
2023-02-08 22:39:17 -0300

    ci: :green_heart: Add deploy workflow fix

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/educacross-chalenge/',
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
